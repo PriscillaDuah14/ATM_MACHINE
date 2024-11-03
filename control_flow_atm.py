1f6b7863852cd2f396e97b916e0d90a3c896802b
Cathy <emefayaacatherena@gmail.com>
2024-11-03 20:02:28 +0000

    PIN

---
+++ control_flow_atm.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+ elif choice == '4':
+            # Change PIN
+            current_pin = int(input("Enter your current PIN: "))
+            if current_pin == pin:
+                new_pin = int(input("Enter your new 4-digit PIN: "))
+                if 1000 <= new_pin <= 9999:
+                    confirm_pin = int(input("Confirm your new PIN: "))
+                    if new_pin == confirm_pin:
+                        pin = new_pin
+                        print("Your PIN has been changed successfully.")
+                    else:
+                        print("PINs do not match. Try again.")
+                else:
+                    print("New PIN must be a 4-digit integer.")
+            else:
+                print("Current PIN is incorrect.")
