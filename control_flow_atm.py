611cfff434ecc25bb40f96e765322dcf78fac569
PriscillaDuah14 <bhaeprissy@gmail.com>
2024-11-23 15:09:25 -0800

    final bits

--- control_flow_atm.py
+++ control_flow_atm.py
@@ -1,5 +1,4 @@
 
-import getpass
 
 def main():
     balance = 2500
@@ -68,21 +67,10 @@ def main():
                 
         elif choice == '4':
             # Change PIN
-            current_pin = int(input("Enter your current PIN: "))
-            if current_pin == pin:
-                new_pin = int(input("Enter your new 4-digit PIN: "))
-                if 1000 <= new_pin <= 9999:
-                    confirm_pin = int(input("Confirm your new PIN: "))
-                    if new_pin == confirm_pin:
-                        pin = new_pin
-                        print("Your PIN has been changed successfully.")
-                    else:
-                        print("PINs do not match. Try again.")
-                else:
-                    print("New PIN must be a 4-digit integer.")
-            else:
-              print("Current PIN is incorrect.")
-        
+            import pin
+            pin.current_pin
+               
+            
         #Transaction history
         
         elif choice == '5':
